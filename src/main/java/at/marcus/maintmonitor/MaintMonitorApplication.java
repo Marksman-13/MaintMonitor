b19406bb20b8bbfb42a7c19cdc40c91629a67cb0
Marcus <achmedinderbiev@gmail.com>
2022-06-16 17:19:04 +0200

    Initial commit

---
+++ src/main/java/at/marcus/maintmonitor/MaintMonitorApplication.java
@@ -0,0 +1,13 @@
+package at.marcus.maintmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/marcus/maintmonitor/MaintMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.marcus.maintmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

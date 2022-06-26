0def70475c5373d31df49346548693c76e235130
Marcus <achmedinderbiev@gmail.com>
2022-06-26 17:16:09 +0200

    This commit will:  - add Unit Test for the connection

---
+++ src/test/java/at/marcus/maintmonitor/controller/MaintMonitorControllerTest.java
@@ -0,0 +1,23 @@
+package at.marcus.maintmonitor.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.HttpURLConnection;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaintMonitorControllerTest {
+
+    //https://stackoverflow.com/questions/6467848/how-to-get-http-response-code-for-a-url-in-java
+    @Test
+    void testGetMessage() throws IOException {
+        URL url = new URL("http://localhost:8080/maintMode/");
+        HttpURLConnection connection  = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+        int code = connection.getResponseCode();
+        Assertions.assertEquals(HttpURLConnection.HTTP_OK,code);
+    }
+}
